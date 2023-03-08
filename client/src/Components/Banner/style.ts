2445119e91b89b9cbbd6e93ff1375dcc4ce8099a
JuanParraIV <jmparra1993@gmail.com>
2023-03-08 11:09:07 -0500

    feat: Actualizando Develop

--- client/src/Components/Banner/style.ts
+++ client/src/Components/Banner/style.ts
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 export const CarouselContainer = styled.div`
  ${tw` relative `}`;
 export const CarouselBorder = styled.div`
- ${tw` absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20 `}`;
+ ${tw` absolute w-full h-32 bg-gradient-to-t from-zinc-900 to-transparent bottom-0 z-20 `}`;
 export const CarouselTextContainer = styled.div`${tw`absolute top-1/3 w-full text-center`}`;
 export const CarouselTitle = styled.p`
   ${tw`text-white text-2xl lg:text-5xl mb-5`}`;
